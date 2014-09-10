91756623bac40216f1894d8a23fd7b829c3ec413
feguson <fabian@pymetic.mx>
2014-09-09 19:03:06 -0600

    yaquivuelo - createvent
    fixing bug

--- www/js/index.js
+++ www/js/index.js
@@ -51,25 +51,12 @@ var app = {
 };
 
 
-$.event.special.tap = {
-  setup: function() {
-    var self = this,
-      $self = $(self);
+window.onload = function() {
+ $("#area").on("tap",function(){
+    setTimeout(function(){
+                        CreateEvents();
 
-    // Bind touch start
-    $self.on('touchstart', function(startEvent) {
-      // Save the target element of the start event
-      var target = startEvent.target;
+                        }, 300);
 
-      // When a touch starts, bind a touch end handler exactly once,
-      $self.one('touchend', function(endEvent) {
-        // When the touch end event fires, check if the target of the
-        // touch end is the same as the target of the start, and if
-        // so, fire a click.
-        if (target == endEvent.target) {
-          $.event.simulate('tap', self, endEvent);
-        }
-      });
-    });
-  }
+    }
 };
